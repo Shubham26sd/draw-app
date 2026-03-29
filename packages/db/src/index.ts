c2c020de255189591fb732c5d3d051e10d5cbaed
Shubham26sd <shubhamdas.aj97@gmail.com>
2026-03-29 21:07:58 +0530

    added prisma db package

---
+++ packages/db/src/index.ts
@@ -0,0 +1,27 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client.js";
+import "dotenv/config"
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+    throw new Error("DATABASE_URL is not set");
+}
+
+const adapter = new PrismaPg({ connectionString });
+
+const prismaSingleton = () => {
+    return new PrismaClient({ adapter })
+}
+
+declare global {
+    var prisma: undefined | ReturnType<typeof prismaSingleton>
+}
+
+const prismaClient = globalThis.prisma ?? prismaSingleton()
+
+if (process.env.NODE_ENV !== 'production') {
+    globalThis.prisma = prismaClient
+}
+
+export { prismaClient };
