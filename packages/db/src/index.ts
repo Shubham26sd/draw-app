c2c64070ee58bd12ca70a6350fbf002758a35a2d
Shubham26sd <shubhamdas.aj97@gmail.com>
2026-03-30 19:24:07 +0530

    modified_db_package

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config"
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma/client.ts";
+//@ts-ignore
+import { PrismaClient } from "./generated/prisma/client.ts"
 import "dotenv/config"
 
 const connectionString = process.env.DATABASE_URL;
