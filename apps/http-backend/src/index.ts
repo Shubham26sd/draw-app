370bd3b7bb64cbc9146f602c5c7758b3c134deff
Shubham26sd <shubhamdas.aj97@gmail.com>
2026-03-31 17:36:51 +0530

    .

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -138,4 +138,17 @@ app.get("/chats/:roomId", async (req, res) => {
     })
 })
 
+app.get("/room/:slug", async (req, res) => {
+    const slug = req.params.slug
+    const room = await prismaClient.room.findFirst({
+        where: {
+            slug
+        }
+    })
+
+    return res.json({
+        room
+    })
+})
+
 app.listen(3001);
